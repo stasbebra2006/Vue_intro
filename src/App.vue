4e31b5c06832c624a3adfea1d396e57b213d4e9a
Stanislav Liashevskyi <liashevskiys@gmail.com>
2024-07-22 15:05:36 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,59 @@
 <template>
-	<h1>Shopping List</h1>
-	<input v-model="input" @keyup.enter="addItem()" /><button @click="addItem()">Add to list</button>
-	<h2>Items</h2>
-	<ul>
-		<li v-for="item in validItems" :key="item.id"><span @click="deleteItem(item)"
-				style="margin-right: 15px; cursor: pointer;">X</span><span>{{ item.text }}</span>
-		</li>
-	</ul>
-	<h2>Deleted Items</h2>
-	<ul>
-		<li v-for="item in deletedItems" :key="item.id"><span @click="eraseItem(item.id)"
-				style="margin-right: 15px; cursor: pointer;">X</span><span>{{ item.text }}</span>
-		</li>
-	</ul>
+	<div class="main">
+		<h1>Shopping List</h1>
+		<input v-model="newItemName" @keyup.enter="addItem()" />
+		<button @click="addItem()">Add to list</button>
+		<h2>Items</h2>
+		<ul class="item">
+			<li v-for="item in validItems" :key="item.id" class="item-text-size">
+				<span @click="deleteItem(item)" class="delete-button">X</span>
+				<span>{{ item.text }}</span>
+			</li>
+		</ul>
+		<h2>Deleted Items</h2>
+		<ul class="item">
+			<li v-for="item in deletedItems" :key="item.id" class="item-text-size">
+				<span @click="eraseItem(item.id)" class="delete-button">X</span>
+				<span>{{ item.text }}</span>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			input: '',
-			list: []
+			newItemName: '',
+			itemList: []
 		}
 	},
 	computed: {
 		validItems() {
-			return this.list.filter(item => !item.is_deleted)
+			return this.itemList.filter(item => !item.is_deleted)
 		},
 		deletedItems() {
-			return this.list.filter(item => item.is_deleted)
+			return this.itemList.filter(item => item.is_deleted)
 		}
 	},
 	methods: {
 		addItem() {
-			this.list.push({
-				id: this.list.length + 1,
-				text: this.input,
-				is_deleted: false
-			});
-			this.input = ''
+			if (this.newItemName.trim().length > 0) {
+				this.itemList.push({
+					id: this.itemList.length + 1,
+					text: this.newItemName,
+					is_deleted: false
+				});
+				this.newItemName = ''
+			}
 		},
 		deleteItem(item) {
 			item.is_deleted = true
 		},
 		eraseItem(itemId) {
-			const index = this.list.findIndex(item => item.id === itemId)
+			const index = this.itemList.findIndex(item => item.id === itemId)
 			if (index !== -1) {
-				this.list.splice(index, 1)
+				this.itemList.splice(index, 1)
 			}
 		}
 	}
@@ -54,7 +61,7 @@ export default {
 </script>
 
 <style>
-#app {
+.main {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
@@ -63,12 +70,18 @@ export default {
 	margin-top: 60px;
 }
 
-li span {
-	font-size: 1.5em !important;
+.item-text-size {
+	font-size: 1.5em;
 }
 
-ul {
+.item {
 	padding-left: 0;
 	list-style-type: none;
+}
+
+.delete-button {
+	margin-right: 15px;
+	cursor: pointer;
+	color: red;
 }
 </style>
